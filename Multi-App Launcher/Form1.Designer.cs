e2df81af253be783865a47bf29b5d244f5161d5c
Nicholas Bly <73457207+NicholasBly@users.noreply.github.com>
2020-10-26 01:02:23 -0400

    Add files via upload

---
+++ Multi-App Launcher/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Multi_App_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(260, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(260, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 102);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(260, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 128);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(260, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 154);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(260, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 180);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(260, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 206);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(260, 20);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(12, 232);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(260, 20);
+            this.textBox9.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add Paths";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Execute Apps";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(163, 327);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Check";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 39);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Check the checkbox if you want the program to\r\nexecute the apps in apps.ini and t" +
+    "hen close itself.\r\nThis can be adjusted in the ini file.";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(37, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 47);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Refresh Lists";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 410);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Multi-App Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Multi-App Launcher/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_App_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!System.IO.File.Exists("apps.ini"))
+                {
+                    var file = System.IO.File.Create("apps.ini");
+                    MessageBox.Show("apps.ini was not found, file was now created", "Created apps.ini", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    file.Close();
+                }
+                string str = System.IO.File.ReadAllText("apps.ini");
+                if (System.IO.File.Exists("apps.ini") && str.Length == 0)
+                {
+                    MessageBox.Show("No Apps Saved in apps.ini!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (System.IO.File.Exists("apps.ini") && str.Length != 0)
+                {
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox1.Text = System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(1).Take(2).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(1).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(1).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox2.Text = System.IO.File.ReadLines("apps.ini").Skip(1).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(3).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(2).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(2).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox3.Text = System.IO.File.ReadLines("apps.ini").Skip(2).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(4).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(3).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(3).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox4.Text = System.IO.File.ReadLines("apps.ini").Skip(3).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(5).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(4).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(4).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox5.Text = System.IO.File.ReadLines("apps.ini").Skip(4).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(6).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(5).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(5).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox6.Text = System.IO.File.ReadLines("apps.ini").Skip(5).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(7).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(6).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(6).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox7.Text = System.IO.File.ReadLines("apps.ini").Skip(6).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(8).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(7).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(7).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox8.Text = System.IO.File.ReadLines("apps.ini").Skip(7).Take(1).FirstOrDefault();
+                    if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(9).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(8).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(8).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                        textBox9.Text = System.IO.File.ReadLines("apps.ini").Skip(8).Take(1).FirstOrDefault();
+                }
+                if(str.Contains("ExecuteThenClose = 1"))
+                {
+                    checkBox1.Checked = true;
+                }
+                else if (!str.Contains("ExecuteThenClose = 1"))
+                {
+                    checkBox1.Checked = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: \n\n" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+        }
+
+        void launch()
+        {
+            try
+            {
+            if (System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(0).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(1).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(1).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(1).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(1).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(2).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(2).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(2).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(2).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(3).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(3).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(3).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(3).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(4).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(4).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(4).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(4).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(5).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(5).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(5).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(5).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(6).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(6).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(6).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(6).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(7).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(7).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(7).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(7).Take(1).FirstOrDefault());
+
+            if (System.IO.File.ReadLines("apps.ini").Skip(8).Take(1).FirstOrDefault() != null && System.IO.File.ReadLines("apps.ini").Skip(8).Take(1).FirstOrDefault() != "ExecuteThenClose = 1" && System.IO.File.ReadLines("apps.ini").Skip(8).Take(1).FirstOrDefault() != "ExecuteThenClose = 0")
+                    System.Diagnostics.Process.Start(System.IO.File.ReadLines("apps.ini").Skip(8).Take(1).FirstOrDefault());
+            }
+            catch
+            {
+                MessageBox.Show("Error, you probably didn't add the path correctly. Fix it and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            launch();
+            var textx = System.IO.File.ReadAllText("apps.ini");
+            if (textx.Contains("ExecuteThenClose = 1"))
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.CheckState == CheckState.Checked)
+            {
+                if (!System.IO.File.ReadAllText("apps.ini").Contains("ExecuteThenClose = 1"))
+                {
+                    using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter("apps.ini", true))
+                    {
+                        file.WriteLine("");
+                        file.WriteLine("ExecuteThenClose = 1");
+                        file.Close();
+                        file.Dispose();
+                    }
+                }
+            }
+            if (checkBox1.CheckState == CheckState.Unchecked)
+            {
+                if (System.IO.File.ReadAllText("apps.ini").Contains("ExecuteThenClose = 1"))
+                {
+                    MessageBox.Show("Unchecked: please change the 1 to a 0 in the ini.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("To Add Paths, Edit the apps.ini file directly. Shall I open the file for you?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if(result == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("apps.ini");
+            }
+            else if(result == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1_Load(this, null);
+        }
+    }
+}
